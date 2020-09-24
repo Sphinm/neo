537632174766babc8ab9d5c91160d13db15915cc
sumin <1500160640@qq.com>
2020-09-24 23:22:23 +0800

    '修改文件夹大小写done'

--- neo-fe/src/pages/router.entry.tsx
+++ neo-fe/src/pages/router.entry.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react'
 import createLazy from '@/libs/lazy'
-import Main from '@/pages/main1'
+import Main from '@/pages/main'
 import { Redirect, Route, Switch } from 'react-router-dom'
 
 const RoutesEntry = () => (
   <Switch>
     <Route exact path="/" render={() => <Redirect to="/login" />} />
-    <Route exact path="/login" component={createLazy(() => import('@/pages/login1'))} />
+    <Route exact path="/login" component={createLazy(() => import('@/pages/login'))} />
     <Route exact path="/main/:page/:subPage/:subPage2*" component={Main} />
     <Route exact path="/main/:page/:subPage*" component={Main} />
     <Route exact path="/main/:page*" component={Main} />
