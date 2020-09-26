256db3ea23dff26b3c9376b962e668c115e771f9
sumin <1500160640@qq.com>
2020-09-26 14:43:49 +0800

    fix: lottery 调整

--- neo-fe/config-overrides.js
+++ neo-fe/config-overrides.js
@@ -16,7 +16,7 @@ const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin')
 
 const devProxy = () => config => {
   config.before = app => {
-    apiMocker(app, resolve('src/mock/index.js'))
+    // apiMocker(app, resolve('src/mock/index.js'))
   }
   config.proxy = {
     '/api': {
