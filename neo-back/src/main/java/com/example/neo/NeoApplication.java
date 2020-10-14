d6a31149aa4a68243f08970f345c67da1b68246b
sumin <1500160640@qq.com>
2020-10-14 22:31:18 +0800

    feat: update makefile

--- neo-back/src/main/java/com/example/neo/NeoApplication.java
+++ neo-back/src/main/java/com/example/neo/NeoApplication.java
@@ -1,9 +1,11 @@
 package com.example.neo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.neo.**.mapper")
 public class NeoApplication {
     public static void main(String[] args) {
         SpringApplication.run(NeoApplication.class, args);
