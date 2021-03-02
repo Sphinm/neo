394d217a77f72bccab0917ac3c494eacf6040aab
sumin <1500160640@qq.com>
2021-03-02 16:27:29 +0800

    fix: 完税凭证添加逻辑

--- neo-fe/src/apis/merchant.ts
+++ neo-fe/src/apis/merchant.ts
@@ -1,10 +1,5 @@
 import ajax from '@/libs/axios'
 
-// /** 更改密码 */
-// export const changePwd = (params: { oldPwd: string; newPwd: string }) => {
-//   return ajax.post(`/change/password`, params)
-// }
-
 /** 获取代理商返佣 */
 export const fetchMerchantRebate = () => {
   return ajax.get(`/rebate`)
